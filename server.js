ca664ed385ccbb2792610e976827e33c1317b69a
TaliZi <talizi1234@gmail.com>
2024-03-13 18:14:53 +0200

    update - likes and edit posts

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
 const jwt = require("jsonwebtoken");
+
+const { jwtDecode } = require("jwt-decode");
+const { Post, User } = require("./models");
 
 const verifyToken = (req, res, next) => {
   const token = req.headers.authorization;
@@ -13,19 +15,23 @@ const verifyToken = (req, res, next) => {
     return res.status(401).json({ error: "No token provided" });
   }
 
-  jwt.verify(token, "secret_key", (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ error: "Invalid token" });
-    }
-    req.user = decoded;
-    next();
-  });
+  const tokenString = token.replace("Bearer ", ""); // Remove "Bearer " from the token
+
+  const decoded = jwtDecode(tokenString);
+
+  if (!decoded) {
+    console.error("Error verifying token:", err); // Log the error
+    return res.status(401).json({ error: "Invalid token" });
+  }
+  next();
 };
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Middleware
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -60,6 +66,41 @@ app.post("/api/users", async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+// Like Post Endpoint
+app.post("/api/posts/:postId/like", verifyToken, async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const { likes } = req.body;
+    // Find the post by ID and update the likes array to add the user's email
+    const updatedPost = await Post.findByIdAndUpdate(postId, { likes: likes });
+
+    res.json({ message: "Post liked successfully", post: updatedPost });
+  } catch (error) {
+    console.error("Error liking post:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// Unlike Post Endpoint
+app.post("/api/posts/:postId/unlike", verifyToken, async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const { email } = req.body;
+
+    // Find the post by ID and update the likes array to remove the user's email
+    const updatedPost = await Post.findByIdAndUpdate(
+      postId,
+      { $pull: { likes: email } },
+      { new: true }
+    );
+
+    res.json({ message: "Post unliked successfully", post: updatedPost });
+  } catch (error) {
+    console.error("Error unliking post:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 app.get("/api/users/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
@@ -122,21 +163,15 @@ app.get("/api/users/:id/posts", verifyToken, async (req, res) => {
 });
 app.post("/api/users/:id/posts", verifyToken, async (req, res) => {
   try {
-    // Access user information from req.user
-    const userEmail = req.user.email;
-
-    // Check if the requested user is the same as the authenticated user
-    if (req.params.id !== req.user.id) {
-      return res.status(403).json({ error: "Unauthorized access" });
-    }
-
     // Extract post data from the request body
     const { message } = req.body;
+    const { image } = req.body;
 
     // Create a new post
     const newPost = new Post({
       user: req.params.id,
       message,
+      image,
     });
 
     // Save the new post to the database
@@ -149,18 +184,10 @@ app.post("/api/users/:id/posts", verifyToken, async (req, res) => {
     console.error("Error creating post:", error);
     res.status(500).json({ error: "Internal server error" });
   }
-});
+}); // Update Post Endpoint
 app.put("/api/users/:id/posts/:pid", verifyToken, async (req, res) => {
   try {
     // Access user information from req.user
-    const userEmail = req.user.email;
-
-    // Check if the requested user is the same as the authenticated user
-    if (req.params.id !== req.user.id) {
-      return res.status(403).json({ error: "Unauthorized access" });
-    }
-
-    // Extract updated post data from the request body
     const { message } = req.body;
 
     // Find the post by ID and update it
@@ -181,18 +208,10 @@ app.put("/api/users/:id/posts/:pid", verifyToken, async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
-app.delete("/api/users/:id/posts/:pid", verifyToken, async (req, res) => {
+app.delete("/api/users/:id/posts/:postId", verifyToken, async (req, res) => {
   try {
-    // Access user information from req.user
-    const userEmail = req.user.email;
-
-    // Check if the requested user is the same as the authenticated user
-    if (req.params.id !== req.user.id) {
-      return res.status(403).json({ error: "Unauthorized access" });
-    }
-
     // Find the post by ID and delete it
-    const deletedPost = await Post.findByIdAndDelete(req.params.pid);
+    const deletedPost = await Post.findByIdAndDelete(req.params.postId);
 
     // Check if the post exists
     if (!deletedPost) {
@@ -205,102 +224,59 @@ app.delete("/api/users/:id/posts/:pid", verifyToken, async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
-app.get("/api/users/:id/friends", verifyToken, async (req, res) => {
-  try {
-    // Access user information from req.user
-    const userId = req.user.id;
-
-    // Check if the requested user is the same as the authenticated user
-    if (req.params.id !== userId) {
-      return res.status(403).json({ error: "Unauthorized access" });
-    }
-
-    // Fetch the user's friends list from the database
-    const user = await User.findById(userId).populate("friends", "name email");
-
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    res.json({ friends: user.friends });
-  } catch (error) {
-    console.error("Error fetching friends list:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
+// POST /api/users/:id/friends
 app.post("/api/users/:id/friends", verifyToken, async (req, res) => {
   try {
-    // Access user information from req.user
-    const userId = req.user.id;
+    const userId = req.user.id; // Use authenticated user's ID
+    const friendId = req.params.id; // Extract friend's ID from request params
 
-    // Check if the requested user is the same as the authenticated user
-    if (req.params.id !== userId) {
-      return res.status(403).json({ error: "Unauthorized access" });
-    }
-
-    // Extract friend's ID from the request body
-    const { friendId } = req.body;
-
-    // Check if the friend's ID is provided
     if (!friendId) {
       return res.status(400).json({ error: "Friend ID is required" });
     }
 
-    // Check if the friend's ID is valid
     const friend = await User.findById(friendId);
     if (!friend) {
       return res.status(404).json({ error: "Friend not found" });
     }
 
-    // Add the friend to the user's friends list
     const user = await User.findByIdAndUpdate(
       userId,
-      { $addToSet: { friends: friendId } },
+      { $addToSet: { friendRequestsSent: friendId } }, // Update friendRequestsSent array
       { new: true }
     );
 
     res.json({ message: "Friend request sent successfully", user });
   } catch (error) {
-    console.error("Error adding friend:", error);
+    console.error("Error sending friend request:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+// PATCH /api/users/:id/friends/:fid
 app.patch("/api/users/:id/friends/:fid", verifyToken, async (req, res) => {
   try {
-    // Access user information from req.user
     const userId = req.user.id;
-
-    // Check if the requested user is the same as the authenticated user
-    if (req.params.id !== userId) {
-      return res.status(403).json({ error: "Unauthorized access" });
-    }
-
-    // Extract friend's ID from the request parameters
     const friendId = req.params.fid;
 
-    // Find the user and the friend in the database
     const user = await User.findById(userId);
     const friend = await User.findById(friendId);
 
-    // Check if the friend exists
     if (!friend) {
       return res.status(404).json({ error: "Friend not found" });
     }
 
-    // Check if the friend request exists
-    if (!user.friends.includes(friendId)) {
+    if (!user.friendRequestsReceived.includes(friendId)) {
       return res.status(404).json({ error: "Friend request not found" });
     }
 
-    // Confirm the friend request by adding the user to the friend's friends list
     await User.findByIdAndUpdate(
       friendId,
-      { $addToSet: { friends: userId } },
+      { $addToSet: { friends: userId }, $pull: { friendRequestsSent: userId } }, // Add to friends, remove from friendRequestsSent
       { new: true }
     );
 
-    // Remove the friend request from the user's friends list
-    user.friends.pull(friendId);
+    user.friends.push(friendId); // Add friend to user's friends list
+    user.friendRequestsReceived.pull(friendId); // Remove friend request from user's received list
     await user.save();
 
     res.json({ message: "Friend request confirmed successfully" });
@@ -309,28 +285,19 @@ app.patch("/api/users/:id/friends/:fid", verifyToken, async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+// DELETE /api/users/:id/friends/:fid
 app.delete("/api/users/:id/friends/:fid", verifyToken, async (req, res) => {
   try {
-    // Access user information from req.user
     const userId = req.user.id;
-
-    // Check if the requested user is the same as the authenticated user
-    if (req.params.id !== userId) {
-      return res.status(403).json({ error: "Unauthorized access" });
-    }
-
-    // Extract friend's ID from the request parameters
     const friendId = req.params.fid;
 
-    // Find the user in the database
     const user = await User.findById(userId);
 
-    // Check if the friend exists
     if (!user.friends.includes(friendId)) {
       return res.status(404).json({ error: "Friend not found" });
     }
 
-    // Remove the friend from the user's friends list
     user.friends.pull(friendId);
     await user.save();
 
@@ -341,20 +308,34 @@ app.delete("/api/users/:id/friends/:fid", verifyToken, async (req, res) => {
   }
 });
 
-app.get("/api/posts", verifyToken, async (req, res) => {
+app.get("/api/posts/:id", verifyToken, async (req, res) => {
   try {
     // Access user information from req.user
-    const userEmail = req.user.email;
     // Logic to fetch user's feed based on friends
-    const posts = await Post.find({ user: { $in: user.friends } })
-      .sort({ date: -1 })
-      .limit(20);
-    // Additional logic to fetch posts of non-friends
-    const nonFriendPosts = await Post.find({ user: { $nin: user.friends } })
-      .sort({ date: -1 })
-      .limit(5);
+    const userId = req.params.id;
 
-    res.json({ posts: [...posts, ...nonFriendPosts] });
+    // Find the current user's friends
+    const user = await User.findById(userId);
+    const friends = user.friends;
+
+    // Find posts from friends
+    const friendPosts = await Post.find({ user: { $in: friends } })
+      .sort({ date: -1 })
+      .limit(20)
+      .populate("user", "name email image");
+
+    // Find posts from users who are not friends
+    const nonFriendPosts = await Post.find({ user: { $nin: friends } })
+      .sort({ date: -1 })
+      .limit(5)
+      .populate("user", "name email image");
+
+    // Combine and sort all posts
+    const allPosts = friendPosts
+      .concat(nonFriendPosts)
+      .sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    res.json({ posts: allPosts });
   } catch (error) {
     console.error("Error fetching posts:", error);
     res.status(500).json({ error: "Internal server error" });
@@ -382,9 +363,58 @@ app.post("/api/tokens", async (req, res) => {
   }
 });
 // Register route
+app.post("/api/users/:recipientId/friend-request", async (req, res) => {
+  try {
+    const { recipientId } = req.params;
+    const requestingUserId = req.body.user._id; // Assuming you have middleware to extract user ID from JWT
+
+    // Add recipientId to friendRequestsReceived of recipient user
+    await User.findByIdAndUpdate(recipientId, {
+      $addToSet: { friendRequestsReceived: requestingUserId },
+    });
+
+    // Add requestingUserId to friendRequestsSent of requesting user
+    await User.findByIdAndUpdate(requestingUserId, {
+      $addToSet: { friendRequestsSent: recipientId },
+    });
+
+    res.status(204).end();
+  } catch (error) {
+    console.error("Error sending friend request:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Approve Friend Request Endpoint
+app.patch(
+  "/api/users/:requestingUserId/approve-friend-request",
+  async (req, res) => {
+    try {
+      const { requestingUserId } = req.params;
+      const approvingUserId = req.body.user._id; // Assuming you have middleware to extract user ID from JWT
+
+      // Remove requestingUserId from friendRequestsReceived of approving user
+      await User.findByIdAndUpdate(approvingUserId, {
+        $pull: { friendRequestsReceived: requestingUserId },
+      });
+
+      // Add approvingUserId to friends of requesting user
+      await User.findByIdAndUpdate(requestingUserId, {
+        $addToSet: { friends: approvingUserId },
+      });
+      await User.findByIdAndUpdate(approvingUserId, {
+        $addToSet: { friends: requestingUserId },
+      });
+
+      res.status(204).end();
+    } catch (error) {
+      console.error("Error approving friend request:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+);
 app.post("/api/register", async (req, res) => {
   try {
-    console.log("Registering user");
     const { name, email, password, image } = req.body;
     // Check if the email already exists
     const existingUser = await User.findOne({ email });
@@ -393,19 +423,37 @@ app.post("/api/register", async (req, res) => {
     }
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
-    // Create a new user
-    const newUser = new User({
+    // Create a new user with an empty friends array
+    const newUser = {
       name,
       email,
       password: hashedPassword,
       image,
-    });
+    };
+
     // Save the user to the database
-    await newUser.save();
-    res.status(201).json({ message: "User created successfully" });
+    const user = await User.create(newUser);
+
+    res.status(201).json({ message: "User created successfully", user: user });
   } catch (error) {
     console.error("Error registering user:", error);
     res.status(500).json({ error: "Internal server error" });
+  }
+});
+// Modify the backend route to handle GET requests with URL parameters
+app.get("/api/search", async (req, res) => {
+  try {
+    const query = req.query.name; // Extract the value of the "name" parameter from the query string
+
+    // Use the extracted query parameter to search for users by name
+    const users = await User.find({ name: { $regex: query, $options: "i" } });
+
+    // Send back the search results as JSON
+    res.json({ users });
+  } catch (error) {
+    // Handle any errors that occur during the search process
+    console.error("Error searching users:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
@@ -415,6 +463,7 @@ app.post("/api/login", async (req, res) => {
     const { email, password } = req.body;
     // Check if the user exists
     const user = await User.findOne({ email });
+
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
@@ -425,7 +474,7 @@ app.post("/api/login", async (req, res) => {
     }
     // Generate JWT token
     const token = jwt.sign({ email: user.email }, "secret_key");
-    res.json({ token });
+    res.json({ token: token, id: user._id });
   } catch (error) {
     console.error("Error generating token:", error);
     res.status(500).json({ error: "Internal server error" });
